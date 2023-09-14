bd9608a4d8053f299a8720327f8dcdb5dbb615bd
Eric Holk <ericholk@microsoft.com>
2023-09-14 10:14:14 -0700

    Waiting for processes and dropping them works now.

--- src/platform/windows/process.rs
+++ src/platform/windows/process.rs
@@ -3,32 +3,50 @@ use std::{
     process::{self, Child},
 };
 
-use windows::Win32::System::Threading::PROCESS_INFORMATION;
+use windows::Win32::{
+    Foundation::{CloseHandle, WAIT_FAILED},
+    System::Threading::{GetExitCodeProcess, WaitForSingleObject, INFINITE, PROCESS_INFORMATION},
+};
 
 pub struct Process(pub(super) PROCESS_INFORMATION);
 
 impl Process {
     pub fn wait(&mut self) -> io::Result<ExitStatus> {
-        unimplemented!()
+        // SAFETY: FFI calls
+        unsafe {
+            let mut exit_code = 0;
+            let result = WaitForSingleObject(self.0.hProcess, INFINITE);
+            if result == WAIT_FAILED {
+                return Err(io::Error::last_os_error());
+            }
+            if GetExitCodeProcess(self.0.hProcess, &mut exit_code).as_bool() {
+                Ok(ExitStatus::Status(exit_code))
+            } else {
+                Err(io::Error::last_os_error())
+            }
+        }
     }
 }
 
 impl Drop for Process {
     fn drop(&mut self) {
-        // FIXME: close all the handles in PROCESS_INFORMATION
-        unimplemented!()
+        // SAFETY: FFI calls
+        unsafe {
+            CloseHandle(self.0.hProcess);
+            CloseHandle(self.0.hThread);
+        }
     }
 }
 
 #[derive(Debug)]
 pub enum ExitStatus {
-    Status(process::ExitStatus),
+    Status(u32),
 }
 
 impl ExitStatus {
     pub fn success(&self) -> bool {
         match self {
-            ExitStatus::Status(status) => status.success(),
+            ExitStatus::Status(code) => *code == 0,
         }
     }
 }
